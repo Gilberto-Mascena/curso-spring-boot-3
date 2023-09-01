aa8a77049d38d0b5dadc36a225e699e2cca4d2c5
Gilberto Mascena <gilbertomascena@gmail.com>
2023-09-01 19:58:01 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- course/src/main/java/com/gilberto/course/config/TestConfig.java
+++ course/src/main/java/com/gilberto/course/config/TestConfig.java
@@ -12,8 +12,10 @@ import com.gilberto.course.entities.Category;
 import com.gilberto.course.entities.Order;
 import com.gilberto.course.entities.Product;
 import com.gilberto.course.entities.User;
+import com.gilberto.course.entities.enums.OrderItem;
 import com.gilberto.course.entities.enums.OrderStatus;
 import com.gilberto.course.repositories.CategoryRepository;
+import com.gilberto.course.repositories.OrderItemRepository;
 import com.gilberto.course.repositories.OrderRepository;
 import com.gilberto.course.repositories.ProductRepository;
 import com.gilberto.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 		public void run(String... args) throws Exception {
@@ -68,5 +73,12 @@ public class TestConfig implements CommandLineRunner {
 			
 			userRepository.saveAll(Arrays.asList(u1,u2));
 			orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+			
+			OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+			OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+			OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+			OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+			
+			orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		}
 }
--- course/src/main/java/com/gilberto/course/entities/enums/OrderItem.java
+++ course/src/main/java/com/gilberto/course/entities/enums/OrderItem.java
@@ -3,6 +3,7 @@ package com.gilberto.course.entities.enums;
 import java.io.Serializable;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gilberto.course.entities.Order;
 import com.gilberto.course.entities.Product;
 import com.gilberto.course.entities.pk.OrderItemPK;
@@ -17,7 +18,7 @@ public class OrderItem implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	private Integer quantity;
 	private Double price;
@@ -33,6 +34,7 @@ public class OrderItem implements Serializable{
 		this.price = price;
 	}
 	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
